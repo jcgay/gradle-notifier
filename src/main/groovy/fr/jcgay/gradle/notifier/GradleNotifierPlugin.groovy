1055ffc4961b869fefad080b717d41c307a2b1d1
Jean-Christophe GAY <contact@jeanchristophegay.com>
2015-05-09 19:37:21 +0200

    Can pass configuration using system properties
    For example, one can run:
    
        ./gradlew check -Dnotifier.anybar.port=1740
    
    to change port used by AnyBar to indicate build status.
    
    Fixes #4

--- src/main/groovy/fr/jcgay/gradle/notifier/GradleNotifierPlugin.groovy
+++ src/main/groovy/fr/jcgay/gradle/notifier/GradleNotifierPlugin.groovy
@@ -1,15 +1,13 @@
 package fr.jcgay.gradle.notifier
-
 import fr.jcgay.gradle.notifier.extension.Configuration
 import fr.jcgay.gradle.notifier.time.Stopwatch
 import fr.jcgay.notification.Application
 import fr.jcgay.notification.Icon
 import fr.jcgay.notification.Notifier
 import fr.jcgay.notification.SendNotification
+import groovy.transform.PackageScope
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-
-import javax.inject.Inject
 
 class GradleNotifierPlugin implements Plugin<Project> {
 
@@ -17,16 +15,9 @@ class GradleNotifierPlugin implements Plugin<Project> {
     private static final Application APPLICATION = Application.builder('application/x-vnd-gradle-inc.gradle', 'Gradle', GRADLE_ICON).build()
 
     private final Stopwatch stopwatch
-    private final SendNotification factory
 
-    @Inject
     GradleNotifierPlugin() {
-        this(new SendNotification())
-    }
-
-    GradleNotifierPlugin(SendNotification factory) {
         this.stopwatch = Stopwatch.createStarted()
-        this.factory = factory
     }
 
     @Override
@@ -34,14 +25,22 @@ class GradleNotifierPlugin implements Plugin<Project> {
         project.extensions.create('notifier', Configuration)
 
         project.afterEvaluate {
-            Notifier notifier = factory
+            Notifier notifier = new SendNotification()
                 .setApplication(APPLICATION)
-                .addConfigurationProperties(project.notifier.asProperties())
+                .addConfigurationProperties(mergeProperties(project.notifier))
                 .chooseNotifier()
 
             project.gradle.addBuildListener(new NotifierListener(notifier, stopwatch, project.notifier.threshold))
         }
 
+    }
+
+    @PackageScope
+    static Properties mergeProperties(Configuration userConfig) {
+        Properties result = [:]
+        result << userConfig.asProperties()
+        result << System.getProperties().findAll { (it.key as String).startsWith('notifier') }
+        result
     }
 }
 
--- src/test/groovy/fr/jcgay/gradle/notifier/GradleNotifierPluginTest.groovy
+++ src/test/groovy/fr/jcgay/gradle/notifier/GradleNotifierPluginTest.groovy
@@ -1,11 +1,31 @@
 package fr.jcgay.gradle.notifier
 
+import fr.jcgay.gradle.notifier.extension.Configuration
 import nebula.test.PluginProjectSpec
+import org.junit.Rule
+import org.junit.contrib.java.lang.system.ProvideSystemProperty
 
 class GradleNotifierPluginTest extends PluginProjectSpec {
+
+    @Rule
+    ProvideSystemProperty system = new ProvideSystemProperty('notifier.anybar.port', '9999')
+        .and('notifier.implementation', 'anybar')
 
     @Override
     String getPluginName() {
         'fr.jcgay.gradle-notifier'
+    }
+
+    def "should merge user configuration and system properties"() {
+        given:
+        def userConfig = new Configuration()
+        userConfig.anyBar.port = 1739
+
+        when:
+        def result = GradleNotifierPlugin.mergeProperties(userConfig)
+
+        then:
+        result['notifier.implementation'] == 'anybar'
+        result['notifier.anybar.port'] == '9999'
     }
 }
