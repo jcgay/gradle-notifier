205922c74f8edc6dfeb666550d8edc9931a76763
Jean-Christophe Gay <contact@jeanchristophegay.com>
2019-10-19 10:48:59 +0200

    Upgrade to Gradle 5.6.3

--- src/test/groovy/fr/jcgay/gradle/notifier/GradleNotifierPluginIntegrationTest.groovy
+++ src/test/groovy/fr/jcgay/gradle/notifier/GradleNotifierPluginIntegrationTest.groovy
@@ -1,16 +1,16 @@
 package fr.jcgay.gradle.notifier
 
-import static org.gradle.testkit.runner.TaskOutcome.*
 import org.gradle.testkit.runner.GradleRunner
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import static org.gradle.testkit.runner.TaskOutcome.*
 
 class GradleNotifierPluginIntegrationTest extends Specification {
 
-    static def versions = ['4.2.1', '4.3', '4.5.1', '4.10.2', '5.3.1', '5.4']
+    static def versions = ['4.2.1', '4.3', '4.5.1', '4.10.2', '5.3.1', '5.6.3']
 
     @Rule
     TemporaryFolder testProjectDir = new TemporaryFolder()
